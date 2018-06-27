70218847b7a9b2e8de46a52c74754d9e26956494
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-06-27 16:42:16 -0700

    Add Footer component in static components folder

---
+++ jarvis2.0/src/StaticComponents/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+// import "./images";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img
+        href="https://github.com/beckymcclain8/Jarvis2.0"
+        target="_blank"
+        rel="noopener noreferrer"
+        src="https://image.flaticon.com/icons/svg/38/38401.svg"
+        alt="GitHub"
+      />
+      <h5>&copy; 2018 Jarvis 2.0 </h5>
+    </footer>
+  );
+};
+
+export default Footer;
