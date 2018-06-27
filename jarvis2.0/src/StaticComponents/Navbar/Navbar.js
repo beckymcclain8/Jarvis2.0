d86e60a741b7b8a82f0ff1da0c481585074bdbdf
Becky McClain <beckykelley8@yahoo.com>
2018-06-27 16:22:57 -0700

    navbar in progress

---
+++ jarvis2.0/src/StaticComponents/Navbar/Navbar.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = props => (
+    <nav className="navbar">
+    <Link className="logo" to="/">
+    Jarvis
+    </Link>
+    <div> 
+      <ul className="nav">
+        <li
+          className={
+            window.location.pathname === "/" 
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li
+          className={
+            window.location.pathname === "/map"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/map" className="nav-link">
+            Map
+          </Link>
+        </li>
+        <li
+          className={
+            window.location.pathname === "/search"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/search" className="nav-link">
+            Search
+          </Link>
+        </li>
+        <li
+          className={
+            window.location.pathname === "/patient"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/patient" className="nav-link">
+            Patient
+          </Link>
+        </li>
+        <li
+          className={
+            window.location.pathname === "/provider"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/provider" className="nav-link">
+            Provider
+          </Link>
+        </li>
+        <li
+          className={
+            window.location.pathname === "/logout"
+              ? "nav-item active"
+              : "nav-item"
+          }
+        >
+          <Link to="/logout" className="nav-link">
+            Log Out
+          </Link>
+        </li>
+      </ul>
+    </div>
+    </nav>
+);
+
+export default Navbar;
