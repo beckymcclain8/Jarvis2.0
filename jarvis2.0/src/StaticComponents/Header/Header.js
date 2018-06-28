2606181ea1daa1348ad46b878ed5bc14c0588849
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-06-27 17:06:44 -0700

    basic app.css colors for dev layouts

---
+++ jarvis2.0/src/StaticComponents/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Header.css";
+import "../../index.css";
+
+const Header = props => {
+  return <div className="header">Jarvis</div>;
+};
+
+export default Header;
