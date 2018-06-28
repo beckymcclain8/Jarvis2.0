bb6714c2306ddd8252a5ea0066a2039b6bd3ba85
Patrick Miles <patrick.miles@gmail.com>
2018-06-28 14:54:18 -0700

    API for getting hospital data

--- jarvis2.0/src/utils/API.js
+++ jarvis2.0/src/utils/API.js
@@ -1,4 +1,25 @@
-// * {
-//     margin: 0;
-//     padding: 0;
-//   }
+import React from 'react';
+import axios from 'axios';
+
+export default class Result extends React.Component {
+  state = {
+    hospitals: []
+  }
+
+  componentDidMount() {
+    axios.get("https://data.cms.gov/resource/ehrv-m9r6.json?$where=provider_zip_code in("+userzips+")")
+      .then(res => {
+        const hospitals = res.data;
+        this.setState({ hospitals });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.hospitals.map(hospital => <li>{hospital.provider_name}</li>
+    <li>{hospital.provider_street_address}, {hospital.provider_state} {hospital.provider_zip_code}</li>)})}
+      </ul>
+    )
+  }
+}
