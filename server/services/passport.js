6629b1f5e66289eb5f930c1fb5fc6c5452c9ed49
knees <you@example.com>
2018-06-29 08:54:35 -0700

    google oauth

---
+++ server/services/passport.js
@@ -0,0 +1,15 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
+//            creates new instance of googleStrategy
+const User = mongoose.model("users");
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback"
+}, (accessToken, refreshToken, profile, done) => {
+  new User({ googleID: profile.id }).save();
+})
+);
