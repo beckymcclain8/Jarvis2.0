99d0db25ce582f765b23fe1a1f779b5aebef852f
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-07-04 13:15:37 -0700

    Fixing the map render and adding two test react components from react-leaflet as well as adding todo notes

--- jarvis2.0/src/pages/MapViewddd/MapView.js
+++ jarvis2.0/src/pages/MapViewddd/MapView.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+// import { Map, LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
 import "../../App.css";
 import "../../index.css";
-import "./Map.css";
-import "./MapStyle.css";
+import "./MapView.css";
+import "./MapViewStyle.css";
 import Navbar from "../../StaticComponents/Navbar";
 import Header from "../../components/Header";
 import Footer from "../../StaticComponents/Footer";
@@ -15,31 +16,26 @@ class WrappedMarker extends Component {
       <Marker
         map={map} /* pass down to Marker */
         layerContainer={layerContainer} /* pass down to Marker */
-        position={[51.505, -0.09]} //This will render the lat/long from the geoJSON TODO|
-        //Test with declaring position as this.props, and then using map() to produce an array of markers on the screen
-        //Then create route to findAll({}) on a test GeoJSON layer that I load up.
+        position={[51.505, -0.09]}
       />
     );
   }
 }
 
-class MapView extends Component {
+export default class MapView extends Component {
   render() {
     return (
       <div className="container">
         <Navbar />
         <Header />
-        {/* Map will center on the the persons location- TODO */}
-        <Map id="map" center={[51.505, -0.09]} zoom={13}>
-          <WrappedMarker />
-        </Map>
         <div className="aside">
           RADIUS HOSPITAL RESULTS OF THE USER GO HERE{" "}
         </div>
+        <Map id="map" center={[51.505, -0.09]} zoom={13}>
+          <WrappedMarker />
+        </Map>
         <Footer />
       </div>
     );
   }
 }
-
-export default MapView;
