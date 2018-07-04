cb88d4031fcdf628e13f73388c7ea85f263855eb
Becky McClain <beckykelley8@yahoo.com>
2018-07-03 20:51:40 -0700

    provider page started

--- jarvis2.0/src/components/ContactBadge/ContactBadge.js
+++ jarvis2.0/src/components/ContactBadge/ContactBadge.js
@@ -3,8 +3,15 @@ import "./ContactBadge.css";
 import "../../App.css";
 import "../../index.css";
 
-const ContactBadge = props => {
-  return <div className="contactBadge">Contact Badge</div>;
-};
+const ContactBadge = props => (
+
+  <div className={props.className} {...props}>
+  <h1>{props.genre}</h1>
+  <h2>{props.doctor}</h2>
+  <h3>{props.address}</h3>
+  <h3>{props.rating}</h3>
+ 
+  </div>
+);
 
 export default ContactBadge;
--- jarvis2.0/src/pages/Provider/Provider.js
+++ jarvis2.0/src/pages/Provider/Provider.js
@@ -1,3 +1,36 @@
 import React, { Component } from "react";
 import "../../App.css";
 import "../../index.css";
+import "./Provider.css";
+import "./ProviderStyle.css";
+import Navbar from "../../StaticComponents/Navbar";
+import Header from "../../components/Header";
+import Footer from "../../StaticComponents/Footer";
+import StarVote from "../../components/StarVotes";
+import ContactBadge from "../../components/ContactBadge";
+
+class Provider extends Component {
+
+render() {
+    return (
+        <div className="container">
+        <Navbar />
+        <Header />
+        <h1 id="fav">Recommended Partners</h1>
+        <h1 id="greeting">Welcome to Dr. McClain's Page!</h1>
+        <StarVote />
+        <ContactBadge 
+        className="contactBadge"
+        genre="Physical Therapy"
+        doctor="Dr. McClain"
+        address="123 Get Better Way"
+        rating="Five Stars" 
+        />
+        <Footer />
+        </div>
+    );
+}
+
+}
+
+export default Provider;
