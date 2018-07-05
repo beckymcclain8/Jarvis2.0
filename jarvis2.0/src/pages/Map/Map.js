65965edc3e345e4464e25dca9cbce89c8e93cac8
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-07-04 19:43:15 -0700

    Add in the map function that can take an array of lat/long.TODO is to make it take an array of object so that I can retrieve hospital information to display in the pop ups

--- jarvis2.0/src/pages/Map/Map.js
+++ jarvis2.0/src/pages/Map/Map.js
@@ -6,24 +6,31 @@ import "./MapStyle.css";
 import Navbar from "../../StaticComponents/Navbar";
 import Header from "../../components/Header";
 import Footer from "../../StaticComponents/Footer";
-import { Map, Marker } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
-class WrappedMarker extends Component {
-  render() {
-    const { map, layerContainer } = this.props; //Given by the `Map` component
-    return (
-      <Marker
-        map={map} /* pass down to Marker */
-        layerContainer={layerContainer} /* pass down to Marker */
-        position={[51.505, -0.09]} //This will render the lat/long from the geoJSON TODO|
-        //Test with declaring position as this.props, and then using map() to produce an array of markers on the screen
-        //Then create route to findAll({}) on a test GeoJSON layer that I load up.
-      />
-    );
-  }
-}
+// class WrappedMarker extends Component {
+//   render() {
+//     const { map, layerContainer } = this.props; //Given by the `Map` component
+//     return (
+//       <Marker
+//         map={map} /* pass down to Marker */
+//         layerContainer={layerContainer} /* pass down to Marker */
+//         position={[51.505, -0.09]} //This will render the lat/long from the geoJSON TODO|
+//         //Test with declaring position as this.props, and then using map() to produce an array of markers on the screen
+//         //Then create route to findAll({}) on a test GeoJSON layer that I load up.
+//       />
+//     );
+//   }
+// }
 
 class MapView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      markers: [[51.505, -0.09], [51.505, -0.08], [51.505, -0.07]]
+    };
+  }
+
   render() {
     return (
       <div className="container">
@@ -31,7 +38,20 @@ class MapView extends Component {
         <Header />
         {/* Map will center on the the persons location- TODO */}
         <Map id="map" center={[51.505, -0.09]} zoom={13}>
-          <WrappedMarker />
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* Take in position as an argument and then map through the state array and called each elements lat/log with the idx operator */}
+          {this.state.markers.map((position, idx) => (
+            <Marker key={`marker-${idx}`} position={position}>
+              <Popup>
+                <span>
+                  Some Awesome Hospital Information <br /> Easily customizable.
+                </span>
+              </Popup>
+            </Marker>
+          ))}
         </Map>
         <div className="aside">
           RADIUS HOSPITAL RESULTS OF THE USER GO HERE{" "}
