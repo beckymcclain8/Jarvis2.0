6674472b5e7a11ac3ed7f8a99d4ba8ae93a88024
knees <you@example.com>
2018-07-07 13:17:43 -0700

    rerouted folders

--- server/jarvis2.0/src/pages/Search/Search.js
+++ server/jarvis2.0/src/pages/Search/Search.js
@@ -4,7 +4,7 @@ import "../../App.css";
 import "../../index.css";
 import Navbar from "../../StaticComponents/Navbar";
 import Header from "../../components/Header";
-import Carousel from "../../components/Carousel";
+// import Carousel from "../../components/Carousel";
 import Footer from "../../StaticComponents/Footer";
 import FormInput from "../../components/FormInput/FormInput";
 import SearchResultContainer from "../../components/SearchResultContainer/SearchResultConatiner";
