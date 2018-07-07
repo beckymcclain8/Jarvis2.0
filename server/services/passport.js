942854ebe6800c4783d85249abbb14997f22d42e
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-07-07 09:55:16 -0700

    Add in passport.js changes from Anabal

--- server/services/passport.js
+++ server/services/passport.js
@@ -10,29 +10,37 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-  .then(user => {
+  User.findById(id).then(user => {
     done(null, user);
-  })
+  });
 });
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: "/auth/google/callback"
-}, (accessToken, refreshToken, profile, done) => {
-User.findOne({ googleID: profile.id })
-.then(existingUser => {
-  if(existingUser) {
-//we already have a record with the given profile ID
-done(null, existingUser);
-  } else {
-//we dont have a user record with this ID, make new record
-new User({ googleID: profile.id })
-.save()
-.then(user => done(null, user));
-  }
-});
-}
-)
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      // console.log(profile.displayName)
+      const existingUser = await User.findOne({
+        googleID: profile.id,
+        googleName: profile.displayName
+      });
+
+      if (existingUser) {
+        console.log(existingUser);
+        //we already have a record with the given profile ID
+        return done(null, existingUser);
+      }
+      //we dont have a user record with this ID, make new record
+      const user = await new User({
+        googleID: profile.id,
+        googleName: profile.displayName
+      }).save();
+      done(null, user);
+    }
+  )
 );
