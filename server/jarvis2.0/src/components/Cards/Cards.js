efad472cf6242c943d81fdd246ca883200a0ea85
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-07-08 18:41:14 -0700

    Add initial folders for redux

---
+++ server/jarvis2.0/src/components/Cards/Cards.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Card, CardHeader, CardBody, CardFooter } from "react-simple-card";
+import SubmitBtn from "../SubmitBtn";
+import "../../App.css";
+import "../../index.css";
+
+const Cards = props => (
+<Card className = {props.className}>
+    <CardHeader> 
+        <h2>{ props.hospital } </h2>
+    </CardHeader>
+    <CardBody>
+      {/* <h4> {props.address} </h4> */}
+      <h4> { props.city } </h4>
+      <h4> Distance: { props.distance } </h4>
+      <h4> Cost of Procedure: {props.procedureCost } </h4>
+      <h4> Driving Cost: { props.driveCost } </h4>
+      <h2> Total Cost: { props.totalCost } </h2>
+    </CardBody>
+    <CardFooter>
+        Save Button
+        {/* <SubmitBtn> {props.title} </SubmitBtn> */}
+    </CardFooter>
+  </Card>
+);
+
+export default Cards;
