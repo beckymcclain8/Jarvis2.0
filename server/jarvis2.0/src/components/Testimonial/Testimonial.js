8716598fc4fa360ad766aee1b0ea27f3b12a909e
Becky McClain <beckykelley8@yahoo.com>
2018-07-09 16:27:43 -0700

    ratings and cards

--- server/jarvis2.0/src/components/Testimonial/Testimonial.js
+++ server/jarvis2.0/src/components/Testimonial/Testimonial.js
@@ -4,7 +4,9 @@ import "../../App.css";
 import "../../index.css";
 
 const Testimonial = props => {
-  return <div className="testimonial">Testimonial</div>;
+  return <div className="testimonial">
+  <h5 id="test">{props.testimonial}</h5>
+  </div>;
 };
 
 export default Testimonial; 
--- server/jarvis2.0/src/pages/Search/Search.js
+++ server/jarvis2.0/src/pages/Search/Search.js
@@ -21,8 +21,8 @@ class Search extends Component {
         <Navbar />
         <Header />
 
-<FormInput />
-<SearchResultContainer />
+        <FormInput />
+        <SearchResultContainer />
 
         <Footer />
       </div>
