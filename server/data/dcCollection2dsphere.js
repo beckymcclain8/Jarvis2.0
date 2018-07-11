3858c0c5c2a22933cd31a122bf2286edac11784b
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-07-10 18:14:30 -0700

    Add in map markers model

---
+++ server/data/dcCollection2dsphere.js
@@ -0,0 +1,8 @@
+db.getCollection("new418").ensureIndex({location:'2dsphere'});
+db.getCollection("new419").ensureIndex({location:'2dsphere'});
+db.getCollection("new469").ensureIndex({location:'2dsphere'});
+db.getCollection("new470").ensureIndex({location:'2dsphere'});
+db.getCollection("new482").ensureIndex({location:'2dsphere'});
+db.getCollection("new638").ensureIndex({location:'2dsphere'});
+db.getCollection("new897").ensureIndex({location:'2dsphere'});
+
