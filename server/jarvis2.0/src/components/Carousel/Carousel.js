7c1ab2070c72da5f28b9b6fe4bea9d4b0eaae86b
Becky McClain <beckykelley8@yahoo.com>
2018-07-12 16:09:17 -0700

    added words to carousel

--- server/jarvis2.0/src/components/Carousel/Carousel.js
+++ server/jarvis2.0/src/components/Carousel/Carousel.js
@@ -17,12 +17,27 @@ const Carousel = props => (
   <div className="carousel-inner">
     <div className="carousel-item active">
       <img className="d-block w-100" src="https://image.freepik.com/free-photo/blur-hospital_1203-7956.jpg" alt="First slide"/>
+      <div class="carousel-caption d-none d-md-block">
+      <h1 className="car1"> Jarvis was quick and easy to use and helped me find the right hospital. </h1>
+      <br></br>
+      <h3 className="car1"> - Jack Anderson, California </h3>
+      </div>
     </div>
     <div className="carousel-item">
-      <img className="d-block w-100" src="https://image.freepik.com/free-photo/doctor-with-a-stethoscope-in-the-hands-and-hospital-background_1423-1.jpg" alt="Second slide"/>
+      <img className="d-block w-100" src="http://backgroundcheckall.com/wp-content/uploads/2017/12/hospital-background-7.jpg" alt="Second slide"/>
+      <div class="carousel-caption d-none d-md-block">
+      <h1 className="car2"> I was able to save thousands of dollars by driving to a hospital just a few hours away. </h1>
+      <br></br>
+      <h3 className="car2"> - Don Jones, Iowa </h3>
+      </div>
     </div>
     <div className="carousel-item">
-      <img className="d-block w-100" src="https://image.freepik.com/free-photo/doctor-s-stethoscope-with-blue-background_23-2147652363.jpg" alt="Third slide"/>
+      <img className="d-block w-100" src="https://image.freepik.com/free-photo/blurred-abstract-background-interior-view-looking-out-toward-to-empty-office-lobby-and-entrance-doors-and-glass-curtain-wall-with-frame_1339-6352.jpg" alt="Third slide"/>
+      <div class="carousel-caption d-none d-md-block">
+      <h1 className="car2"> I was able to save thousands of dollars by driving to a hospital just a few hours away. </h1>
+      <br></br>
+      <h3 className="car2"> - Don Jones, Iowa </h3>
+      </div>
     </div>
   </div>
   {/* <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
