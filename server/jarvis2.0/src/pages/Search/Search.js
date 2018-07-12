415950921d4b1da243e2b98e6fd7492298f597ca
Patrick Miles <patrick.miles@gmail.com>
2018-07-12 14:45:53 -0700

    updated search page

--- server/jarvis2.0/src/pages/Search/Search.js
+++ server/jarvis2.0/src/pages/Search/Search.js
@@ -4,24 +4,18 @@ import "../../App.css";
 import "../../index.css";
 import Navbar from "../../StaticComponents/Navbar";
 import Header from "../../components/Header";
-// import Carousel from "../../components/Carousel";
 import Footer from "../../StaticComponents/Footer";
 import FormInput from "../../components/FormInput/FormInput";
 import SearchResultContainer from "../../components/SearchResultContainer/SearchResultConatiner";
 
 class Search extends Component {
-  scroll = () => {
-    //go to /search page
-    console.log("Scroll the carousel");
-  };
-
+ 
   render() {
     return (
       <div className="container">
         <Navbar />
         <Header />
 
-        <FormInput />
         <SearchResultContainer />
 
         <Footer />
