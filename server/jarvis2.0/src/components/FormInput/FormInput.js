c5ec6c83d2acdba6e301cdc239e8e304c943daec
pjmiles2 <33292834+pjmiles2@users.noreply.github.com>
2018-07-11 23:09:16 -0700

    Update FormInput.js

--- server/jarvis2.0/src/components/FormInput/FormInput.js
+++ server/jarvis2.0/src/components/FormInput/FormInput.js
@@ -1,95 +1,78 @@
-import "./FormInput.css";
-import "../../App.css";
-import "../../index.css";
+import React from "react";
 
-import React, { Component } from "react";
-import "./FormInput.css";
-
-class FormInput extends Component {
-  // Setting the component's initial state
-  state = {
-    address: "",
-    city: "",
-    state: "",
-    zipCode: "",
-    radius: "", 
-    procedure: ""
-  };
-
-  handleInputChange = event => {
-    // Getting the value and name of the input which triggered the change
-    const { name, value } = event.target;
-
-    // Updating the input's state
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    event.preventDefault();
-
-  };
-
-  render() {
-    // Notice how each input has a `value`, `name`, and `onChange` prop
-    return (     
-        <form className="form">
-          <input
-          className="address"
-            value={this.state.address}
-            name="address"
-            onChange={this.handleInputChange}
-            type="text"
-            placeholder="Enter your address"
-          />
-          <br></br>
-          <input
-            value={this.state.city}
-            name="city"
-            onChange={this.handleInputChange}
-            type="text"
-            placeholder="City"
-          />
-          <br></br>
-          <input
-            value={this.state.state}
-            name="state"
-            onChange={this.handleInputChange}
-            type="text"
-            placeholder="State"
-          />
-          <br></br>
-           <input
-            value={this.state.zipCode}
-            name="zipCode"
-            onChange={this.handleInputChange}
-            type="text"
-            placeholder="Zip Code"
-          />
-          <br></br>
-            <input
-            value={this.state.radius}
-            name="radius"
-            onChange={this.handleInputChange}
-            type="text"
-            placeholder="Search Radius"
-          />
-          <br></br>
-            <input
-            value={this.state.procedure}
-            name="procedure"
-            onChange={this.handleInputChange}
-            type="text"
-            placeholder="Procedure list here"
-          />
-          <br></br>
-          <button onClick={this.handleFormSubmit}>Submit</button>
-        </form>
-   
-    );
-  }
-}
+const FormInput = props => (
+  <form>
+    <div className="form-group">
+      <label htmlFor="address">Address:</label>
+      <input
+        onChange={props.handleInputChange}
+        value={props.address}
+        name="address"
+        type="text"
+        className="form-control"
+        placeholder="Enter Your Address"
+        id="address"
+      />
+        <label htmlFor="city">City:</label>
+      <input
+        onChange={props.handleInputChange}
+        value={props.city}
+        name="city"
+        type="text"
+        className="form-control"
+        placeholder="Enter Your City"
+        id="City"
+      />
+        <label htmlFor="state">State:</label>
+      <input
+        onChange={props.handleInputChange}
+        value={props.search}
+        name="state"
+        type="text"
+        className="form-control"
+        placeholder="ST"
+        id="state"
+      />
+        <label htmlFor="zipCode">Zip Code:</label>
+      <input
+        onChange={props.handleInputChange}
+        value={props.search}
+        name="zipCode"
+        type="number"
+        className="form-control"
+        placeholder="Zip Code"
+        id="zipCode"
+      />
+        <label htmlFor="radius">Search Radius:</label>
+      <input
+        onChange={props.handleInputChange}
+        value={props.radius}
+        name="radius"
+        type="number"
+        className="form-control"
+        placeholder="Enter the number of miles you are willing to travel"
+        id="radius"
+      />
+      <label htmlFor="procedure">Procedure:</label>
+      <input
+        onChange={props.handleInputChange}
+        value={props.procedure}
+        name="procedure"
+        type="text"
+        className="form-control"
+        placeholder="Select your procedure"
+        id="procedure"
+      />
+      <button
+        onClick={props.handleFormSubmit}
+        className="btn btn-primary mt-3"
+      >
+        Search
+      </button>
+    </div>
+  </form>
+);
 
 export default FormInput;
+
+
