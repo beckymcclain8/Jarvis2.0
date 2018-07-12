222e5fd06eb8731ea740959616017990bc8162e4
pjmiles2 <33292834+pjmiles2@users.noreply.github.com>
2018-07-12 09:59:01 -0700

    Update Result.js

--- server/jarvis2.0/src/components/Result/Result.js
+++ server/jarvis2.0/src/components/Result/Result.js
@@ -4,8 +4,20 @@ import "../../App.css";
 import "../../index.css";
 
 const Result = props => (
-  <ul className="list-group">
-    {props.results.map(result => (
+ <ul className="list-group">
+  {props.localResult.map(result => (
+    <li className="list-group-item" key={result.id}>
+    <h4>Below is your lowest cost local result:</h4>
+    <h4>Hospital Name: {result.provider_name}</h4>
+    <h4> {result.provider_street_address} </h4>
+    <h4> {result.provider_city}, {result.provider_state} {result.provider_zip_code} </h4>
+    <h4> Cost of Procedure: {result.average_covered_charges} </h4>
+
+    </li>
+  ))}
+    <h4>Below are lower cost options if you choose to travel:</h4>
+
+    {props.moreResults.map(result => (
       <li className="list-group-item" key={result.id}>
       <h4>Hospital Name: {result.provider_name}</h4>
       <h4> {result.provider_street_address} </h4>
