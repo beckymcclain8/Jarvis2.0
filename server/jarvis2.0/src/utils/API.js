fc85ab03139e9710ce4ba3138a8e8d326c213b84
pjmiles2 <33292834+pjmiles2@users.noreply.github.com>
2018-07-12 00:47:48 -0700

    Update API.js

--- server/jarvis2.0/src/utils/API.js
+++ server/jarvis2.0/src/utils/API.js
@@ -5,6 +5,8 @@ const ZIPBASEURL = "https://www.zipcodeapi.com/rest/"
 const ZIPAPIKEY = "6xsv3It4NIGhyQONSzZtJzG35hXBHQg6JJIqO6B7jnv6S8PlAkadIbi6Pg56RUQ2";
 const CORS = "https://cors-anywhere.herokuapp.com/"
 
+
+
 export default {
 
 
@@ -19,6 +21,6 @@ export default {
   
   getHospitals: function(zipRadius) {
 
-    return axios.get(CMSBASEURL + " in("+zipRadius+")&drg_definition=039%20-%20EXTRACRANIAL%20PROCEDURES%20W/O%20CC/MCC");
+    return axios.get(CMSBASEURL + " in("+zipRadius+")&drg_definition=039%20-%20EXTRACRANIAL%20PROCEDURES%20W/O%20CC/MCC&$order=average_covered_charges DESC&$limit=5");
   }
 }
