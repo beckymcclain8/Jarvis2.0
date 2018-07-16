6cb74602a8aa22691b885f2c57802ff0859fe314
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-07-15 23:25:16 -0700

    working backwords to understand why the render in the results page

--- server/jarvis2.0/src/pages/Search/Search.js
+++ server/jarvis2.0/src/pages/Search/Search.js
@@ -7,16 +7,15 @@ import Header from "../../components/Header";
 import Footer from "../../StaticComponents/Footer";
 import FormInput from "../../components/FormInput/FormInput";
 import API from "../../../src/utils/API";
-import Result from "../../../../jarvis2.0/src/components/Result"
+import Result from "../../../../jarvis2.0/src/components/Result";
 
 class Search extends Component {
- 
   state = {
     address: "",
     city: "",
     state: "",
     zipCode: "",
-    radius: "", 
+    radius: "",
     procedure: "",
     localZipRadius: [],
     moreZipRadius: [],
@@ -36,22 +35,30 @@ class Search extends Component {
 
   handleFormSubmit = event => {
     event.preventDefault();
-    this.searchLocalZips(this.state.zipCode, "20")
-    this.searchMoreZips(this.state.zipCode, this.state.radius)
-    console.log(this.state.zipRadius)
-    console.log(this.state.procedure)
-    this.searchLocalHospitals(this.state.localZipRadius)
-    console.log(this.state.lowCost)
-    this.searchMoreHospitals(this.state.moreZipRadius)
+    this.searchLocalZips(this.state.zipCode, "20");
+    this.searchMoreZips(this.state.zipCode, this.state.radius);
+    console.log(this.state.zipRadius);
+    console.log(this.state.procedure);
+    this.searchLocalHospitals(this.state.localZipRadius);
+    console.log(this.state.lowCost);
+    this.searchMoreHospitals(this.state.moreZipRadius);
     this.getDistance();
-    console.log(this.state.distance)
-     };
-  
-  
+    console.log(this.state.distance);
+  };
+
   // componentDidMount() {
   //   this.searchLocalZips(this.state.zipCode, "20");
-    
+
   // }
+  saveHospital = id => {
+    // event.preventDefault();
+    const saveHos = this.state.moreResults.filter(hos => {
+      if (hos.id === id) {
+        console.log(hos);
+      }
+    });
+    console.log(saveHos);
+  };
 
   searchLocalZips = (query, radius) => {
     API.getZips(query, radius)
@@ -83,39 +90,68 @@ class Search extends Component {
   };
 
   getDistance = (userAddress, hospitalAddress) =>
-    API.getDistance(userAddress, hospitalAddress)
-      .then(res => this.setState({distance: res.data.distance}))
+    API.getDistance(userAddress, hospitalAddress).then(res =>
+      this.setState({ distance: res.data.distance })
+    );
 
-  render() { 
-    
+  render() {
     return (
       <div className="container">
-      <Navbar />
-      <Header />
+        <Navbar />
+        <Header />
 
-      {/* <div className="searchGrid" > */}
-      <div id="formID">
-        <FormInput
-          address={this.state.address}
-          city={this.state.city}
-          state={this.state.state}
-          zipCode={this.state.zipCode}
-          radius={this.state.radius}
-          procedure={this.state.procedure.value}
-          handleFormSubmit={this.handleFormSubmit.bind(this)}
-          handleInputChange={this.handleInputChange.bind(this)}
-        />
-      </div>
+        {/* <div className="searchGrid" > */}
+        <div id="formID">
+          <FormInput
+            address={this.state.address}
+            city={this.state.city}
+            state={this.state.state}
+            zipCode={this.state.zipCode}
+            radius={this.state.radius}
+            procedure={this.state.procedure.value}
+            handleFormSubmit={this.handleFormSubmit.bind(this)}
+            handleInputChange={this.handleInputChange.bind(this)}
+          />
+        </div>
 
-      {/* <div id="resultsID"> */}
-        <Result 
-          localResult={this.state.localResult}
-          moreResults={this.state.moreResults}
-        /> 
-      {/* </div> */}
-      {/* </div> */}
-        
-        
+        {/* <div id="resultsID"> */}
+        <div className="localResults">
+          {this.state.localResult.map(result => {
+            <Result
+              id={result.id}
+              provider_name={result.provider_name}
+              provider_street_address={result.provider_street_address}
+              provider_city={result.provider_city}
+              provider_state={result.provider_state}
+              provider_zip_code={result.provider_zip_code}
+              average_covered_charges={result.average_covered_charges}
+              driveCost={250}
+              totalCost={2500}
+              saveHospital={this.saveHospital}
+              className="localResult"
+            />;
+          })}
+        </div>
+        <div className="list-group">
+          {this.state.moreResults.map(result => {
+            <Result
+              id={result.id}
+              provider_name={result.provider_name}
+              provider_street_address={result.provider_street_address}
+              provider_city={result.provider_city}
+              provider_state={result.provider_state}
+              provider_zip_code={result.provider_zip_code}
+              average_covered_charges={result.average_covered_charges}
+              driveCost={250}
+              totalCost={2500}
+              saveHospital={this.saveHospital}
+              className="resultsID"
+            />;
+          })}
+        </div>
+        {/* </div> */}
+        {/* </div> */}
+
         <Footer />
       </div>
     );
--- server/jarvis2.0/src/utils/API.js
+++ server/jarvis2.0/src/utils/API.js
@@ -3,13 +3,14 @@ import axios from "axios";
 const CMSBASEURL =
   "https://data.cms.gov/resource/ehrv-m9r6.json?$where=provider_zip_code";
 const ZIPBASEURL = "https://www.zipcodeapi.com/rest/";
+// const ZIPAPIKEY =
+// "6xsv3It4NIGhyQONSzZtJzG35hXBHQg6JJIqO6B7jnv6S8PlAkadIbi6Pg56RUQ2";
 const ZIPAPIKEY =
-  "6xsv3It4NIGhyQONSzZtJzG35hXBHQg6JJIqO6B7jnv6S8PlAkadIbi6Pg56RUQ2";
-// const ZIPAPIKEY = "jTBXOsUWfV2PlgLc4eObXeW81teBoZhcH3tPeiFCh9oF3tj4l0rnrVDCRe7Zo5y3"
+  "jTBXOsUWfV2PlgLc4eObXeW81teBoZhcH3tPeiFCh9oF3tj4l0rnrVDCRe7Zo5y3";
 const CORS = "https://cors-anywhere.herokuapp.com/";
 
 const MQAPIKey = "UVs4ACBHVSdUdsBxF6ZcdIv1OSmOsM61";
-const MQURL= "http://www.mapquestapi.com/directions/v2/route?key=";
+const MQURL = "http://www.mapquestapi.com/directions/v2/route?key=";
 
 export default {
   getZips: function(query, radius) {
@@ -51,21 +52,29 @@ export default {
   },
 
   getDistance: function(userAddress, hospitalAddress) {
-
-    return axios(CORS+MQURL+MQAPIKey+"&from=3322 E Mariposa St Phoenix AZ 85018&to=2528 E Campbell Ave Phoenix AZ 85016)", {
-      method: 'GET', 
-      headers: { 'x-requested-with': 'whatevs', 
-                  'Content-Type': 'application/json', }, 
-   
-    });
+    return axios(
+      CORS +
+        MQURL +
+        MQAPIKey +
+        "&from=3322 E Mariposa St Phoenix AZ 85018&to=2528 E Campbell Ave Phoenix AZ 85016)",
+      {
+        method: "GET",
+        headers: {
+          "x-requested-with": "whatevs",
+          "Content-Type": "application/json"
+        }
+      }
+    );
   },
-  
-  //Map Exports
-  locateUser: function() {
-    return axios.get("/map");
-  },
 
-  locateNear: function(lnglat) {
-    return axios.get("/map/:lnglat/results");
-  }
+  saveHospital: function() {}
+
+  // //Map Exports
+  // locateUser: function() {
+  //   return axios.get("/map");
+  // },
+
+  // locateNear: function(lnglat) {
+  //   return axios.get("/map/:lnglat/results");
+  // }
 };
